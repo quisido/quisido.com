1230cfca520dd925684dbea08253d1c5e0b6031b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-24 03:22:25 +0000

    Deploy quisido.com to github.com/CharlesStover/quisido.git:gh-pages

---
+++ precache-manifest.c025662c8d4acc6aa264e16d367fb9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5776413433a531b5611e",
+    "url": "/static/js/main.57764134.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e3465e9520bc761fb98c",
+    "url": "/static/js/2.e3465e95.chunk.js"
+  },
+  {
+    "revision": "9a2a17dde5173acd7381961a959e8bdc",
+    "url": "/static/media/charles-stover.9a2a17dd.jpg"
+  },
+  {
+    "revision": "66656232d620f461e39b2d1fe52e1c05",
+    "url": "/static/media/proficiencies.66656232.png"
+  },
+  {
+    "revision": "4f6b19247d85e90cb060b74dac48411f",
+    "url": "/index.html"
+  }
+];
