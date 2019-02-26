eae50f61186bc0c3a9e8200c8fb830ba9de52bce
Charles Stover <github@charlesstover.com>
2019-02-25 17:27:08 -0800

    Update Matt's recommendation

--- src/assets/quotes.js
+++ src/assets/quotes.js
@@ -1,19 +1,23 @@
-import charlesImage from './quotes/charles-stover.jpg';
-import mattImage from './quotes/matt-mcclelland.jpg';
+import charles from './quotes/charles-stover.jpg';
+import matt from './quotes/matt-mcclelland.jpg';
 
 export default [
   {
     author: 'Matt McClelland',
     company: 'Kitestring Technical Services',
-    image: mattImage,
-    quote: 'Ace is an innovative, detail-oriented, and multi-talented artist. Over the last year, I had the pleasure of working with her on a number of projects. From painting models, altering popular MTG cards, and creating custom wooden deck boxes, she has proven time and again to be extraordinarily versatile in her artistic abilities. That’s just the tip of the iceberg when it comes to what she’s capable of.',
-    title: 'Software Developer'
+    image: matt,
+    quote: [
+      'Ace is an innovative, detail-oriented, and multi-talented artist. Over the past year, I have had the pleasure of working with her on a number of projects. From painting models and altering popular MtG cards to creating custom wooden deck boxes, she has proven time and again to be extraordinarily versatile in her artistic abilities. And that\'s just the tip of the iceberg when it comes to what she is capable of.',
+      'She is very receptive to feedback and insightful with her own. Her attention to detail is exceptional and her ability to give each project she works on a unique accent and distinction is truly remarkable.',
+      'She is a trusted and capable individual who would benefit a wide range of businesses with her broad skill set and will continue to be my go-to creative for any future artistic needs.',
+    ],
+    title: 'Software Developer',
   },
   {
     author: 'Charles Stover',
     company: 'Amazon Web Services',
-    image: charlesImage,
+    image: charles,
     quote: 'I have hired Ace on multiple occasions over the last few years to alter and extend existing artwork. Each time, she exceeded my expectations with her outstanding attention to detail and vivid imagery. With very little instruction, she was able to envision, design, and finish each project with better results than I could have hoped. She will remain my go-to artist for a long time to come.',
-    title: 'Front End Engineer II'
+    title: 'Front End Engineer II',
   },
 ];
