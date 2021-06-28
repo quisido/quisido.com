b1bd14f6c28ff0ec09499c8ac86c84dddc325e13
Charles Stover <github@charlesstover.com>
2021-06-28 14:18:54 -0700

    finish mapRefToListItems test

--- src/shared/awsui-icon-pagination/utils/map-ref-to-list-items.test.ts
+++ src/shared/awsui-icon-pagination/utils/map-ref-to-list-items.test.ts
@@ -20,11 +20,9 @@ describe('mapRefToListItems', (): void => {
   it('should throw an error if the UL does not contain 3+ LIs', (): void => {
     const div: HTMLDivElement = document.createElement('div');
     const ul: HTMLUListElement = document.createElement('ul');
-    const nextLi: HTMLLIElement = document.createElement('li');
-    const page1Li: HTMLLIElement = document.createElement('li');
     const previousLi: HTMLLIElement = document.createElement('li');
+    const nextLi: HTMLLIElement = document.createElement('li');
     ul.appendChild(previousLi);
-    ul.appendChild(page1Li);
     ul.appendChild(nextLi);
     div.appendChild(ul);
     expect((): void => {
@@ -32,5 +30,26 @@ describe('mapRefToListItems', (): void => {
         current: div,
       });
     }).toThrowError('Could not find Pagination navigation.');
+  });
+
+  it('should return only page list items', (): void => {
+    const div: HTMLDivElement = document.createElement('div');
+    const ul: HTMLUListElement = document.createElement('ul');
+    const previousLi: HTMLLIElement = document.createElement('li');
+    const page1Li: HTMLLIElement = document.createElement('li');
+    const page2Li: HTMLLIElement = document.createElement('li');
+    const page3Li: HTMLLIElement = document.createElement('li');
+    const nextLi: HTMLLIElement = document.createElement('li');
+    ul.appendChild(previousLi);
+    ul.appendChild(page1Li);
+    ul.appendChild(page2Li);
+    ul.appendChild(page3Li);
+    ul.appendChild(nextLi);
+    div.appendChild(ul);
+    expect(
+      mapRefToListItems({
+        current: div,
+      }),
+    ).toEqual([page1Li, page2Li, page3Li]);
   });
 });
